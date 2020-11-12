e91d45a97e150ef8f13010f052756e2b1bf216e1
HalimHamidov <halimjonik@gmail.com>
2020-11-12 09:35:25 +0300

    feat: add HW2 without cookiemiddleware

---
+++ Services/UserService.cs
@@ -0,0 +1,77 @@
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using WebApi.Entities;
+using WebApi.Helpers;
+using WebApi.Models;
+
+namespace WebApi.Services
+{
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+    }
+
+    public class UserService : IUserService
+    {
+        
+        private List<User> _users = new List<User>
+        {
+            new User { Id = 1, FirstName = "Halim", LastName = "Hamidov", Username = "halim", Password = "sber" }
+        };
+
+        private readonly AppSettings _appSettings;
+
+        public UserService(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public AuthenticateResponse Authenticate(AuthenticateRequest model)
+        {
+            var user = _users.SingleOrDefault(x => x.Username == model.Username && x.Password == model.Password);
+
+            // возвращает null, если пользователь не найден
+            if (user == null) return null;
+
+            // генерация jwt токенов после успешной jwt аутентификации 
+            var token = generateJwtToken(user);
+
+            return new AuthenticateResponse(user, token);
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _users;
+        }
+
+        public User GetById(int id)
+        {
+            return _users.FirstOrDefault(x => x.Id == id);
+        }
+
+        // вспомогательный метод
+
+        private string generateJwtToken(User user)
+        {
+            // генерация токенов которые действительны в течении 30 дней
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddDays(30),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApi.Helpers;
+using WebApi.Services;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // добавить сервисов к контейнеру DI 
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddControllers();
+
+            // настройка объекта строго типизированных параметров
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+
+            // настройки DI для микросервисов 
+            services.AddScoped<IUserService, UserService>();
+        }
+
+        // настройка конвейера HTTP-запросов
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            // Глобальная политика CORS  - технология современных браузеров, которая позволяет предоставить веб-странице доступ к ресурсам другого домена.
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            // реализация jwt auth middleware
+            app.UseMiddleware<JwtMiddleware>();
+
+            app.UseEndpoints(x => x.MapControllers());
+        }
+    }
+}
